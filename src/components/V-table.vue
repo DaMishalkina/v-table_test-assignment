8a8b264a7611432ba68ef5f0f0601c2f87469641
DaMishalkina <dashaio251@gmail.com>
2020-06-23 19:06:02 +0300

    adding .gitignore

---
+++ src/components/V-table.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Hello</p>
+    <v-data-table
+    :headers="headers"
+    >
+
+    </v-data-table>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            headers: [
+                {text: 'ID', align: 'center', value: ''},
+                {text: 'NAME', align: 'center', value: ''},
+                {text: 'TEXT', align: 'center', value: ''}
+            ],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
